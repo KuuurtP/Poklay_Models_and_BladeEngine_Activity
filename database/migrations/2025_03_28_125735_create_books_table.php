4eab8b9ac1e1e6cfa6e7ac5ce83883995aa1a906
KuuurtP <kurt.poklay@gmail.com>
2025-03-28 21:01:50 +0800

    added/edited book table migration

--- database/migrations/2025_03_28_125735_create_books_table.php
+++ database/migrations/2025_03_28_125735_create_books_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('isbn', 13)->unique();
+            $table->string('title', 100);
+            $table->string('author', 100);
+            $table->text('description')->nullable();
+            $table->date('date_published');
         });
     }
 
