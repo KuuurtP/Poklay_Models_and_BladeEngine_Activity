130a6d5388136246ff7c242bc383b6c2cab90c9f
KuuurtP <kurt.poklay@gmail.com>
2025-03-31 17:38:45 +0800

    fixed routes and added index

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-xl font-semibold mb-4">Books</h2>
+    <div class="overflow-x-auto">
+        <table class="min-w-full bg-white shadow rounded">
+            <thead>
+                <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                    <th class="py-3 px-6 text-left">ID</th>
+                    <th class="py-3 px-6 text-left">ISBN</th>
+                    <th class="py-3 px-6 text-left">Title</th>
+                    <th class="py-3 px-6 text-left">Author</th>
+                    <th class="py-3 px-6 text-left">Description</th>
+                    <th class="py-3 px-6 text-left">Date Published</th>
+                </tr>
+            </thead>
+            <tbody class="text-gray-600 text-sm font-light">
+                @foreach($books as $book)
+                    <tr class="border-b border-gray-200 hover:bg-gray-100">
+                        <td class="py-3 px-6 text-left">{{ $book->id }}</td>
+                        <td class="py-3 px-6 text-left">{{ $book->isbn }}</td>
+                        <td class="py-3 px-6 text-left">{{ $book->title }}</td>
+                        <td class="py-3 px-6 text-left">{{ $book->author }}</td>
+                        <td class="py-3 px-6 text-left">{{ $book->description }}</td>
+                        <td class="py-3 px-6 text-left">{{ $book->date_published }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
