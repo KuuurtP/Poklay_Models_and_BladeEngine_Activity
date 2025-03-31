83785cc576106d9771f1796bf103ee2f74ee1f8d
KuuurtP <kurt.poklay@gmail.com>
2025-03-31 17:13:58 +0800

    fetch and pass the books to a view

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        // Fetch all books (consider pagination for larger datasets)
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
 }
