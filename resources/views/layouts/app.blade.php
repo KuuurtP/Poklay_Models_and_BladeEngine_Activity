9e2990243963c87e123c5ef7f373b593c9c521d5
KuuurtP <kurt.poklay@gmail.com>
2025-04-01 14:34:32 +0800

    proper callout for tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book List</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100">
     @include('partials.header')
